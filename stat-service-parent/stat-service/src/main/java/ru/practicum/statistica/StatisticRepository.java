fa635d31442920f475eb7d0fb80b2302aaef161c
Andrey Zinovich <fast.optimus@gmail.com>
2024-07-06 17:04:27 +0300

    swich to java 11

--- stat-service-parent/stat-service/src/main/java/ru/practicum/statistica/StatisticRepository.java
+++ stat-service-parent/stat-service/src/main/java/ru/practicum/statistica/StatisticRepository.java
@@ -12,11 +12,11 @@ import java.util.List;
 @Repository
 public interface StatisticRepository extends JpaRepository<Event, Long> {
 
-    @Query("select new ru.practicum.commons.EventStatisticByEventDto( e.app, e.uri, count(*))" +
-            "from Event as e " +
-            "where e.timestamp between ?1 and ?2" +
-            "and e.uri in (?3)" +
-            "group by e.app, e.uri " +
-            "order by count(*) desc")
+    @Query("select new ru.practicum.commons.EventStatisticByEventDto( e.app, e.uri, count(*)) " +
+            " from Event as e " +
+            " where e.timestamp between ?1 and ?2 " +
+            " and e.uri in (?3) " +
+            " group by e.app, e.uri " +
+            " order by count(*) desc")
     List<EventStatisticByEventDto> countEvents(LocalDateTime from, LocalDateTime to, List<String> uriList);
 }
