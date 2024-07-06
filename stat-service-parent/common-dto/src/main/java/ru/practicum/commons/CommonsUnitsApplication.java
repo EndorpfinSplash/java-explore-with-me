f4fc72e0b23d8888b687752a6737e0b8d3da0912
Andrey Zinovich <fast.optimus@gmail.com>
2024-07-06 12:39:14 +0300

    containers configured

---
+++ stat-service-parent/common-dto/src/main/java/ru/practicum/commons/CommonsUnitsApplication.java
@@ -0,0 +1,12 @@
+package ru.practicum.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonsUnitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonsUnitsApplication.class, args);
+    }
+}
