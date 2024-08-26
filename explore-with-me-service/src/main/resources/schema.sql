be91651a07a62e45452200dff21c217ebe601e7a
Andrey Zinovich <fast.optimus@gmail.com>
2024-08-26 23:24:15 +0300

    feat: development

--- explore-with-me-service/src/main/resources/schema.sql
+++ explore-with-me-service/src/main/resources/schema.sql
@@ -1,7 +1,9 @@
+drop table if exists COMPILATION_EVENT;
+drop table if exists COMPILATION;
 drop table if exists REQUEST;
 drop table if exists EVENT;
-drop table if exists USERS;
 drop table if exists EVENT_CATEGORY;
+drop table if exists USERS;
 
 
 CREATE TABLE IF NOT EXISTS USERS
@@ -59,3 +61,45 @@ CREATE TABLE IF NOT EXISTS COMPILATION_EVENT
     compilation_id bigint references COMPILATION (id) on delete cascade,
     event_id       bigint references EVENT (id) on delete cascade
 );
+
+CREATE OR REPLACE FUNCTION distance(lat1 float, lon1 float, lat2 float, lon2 float)
+    RETURNS float
+AS
+'
+    declare
+        dist float = 0;
+        rad_lat1 float;
+        rad_lat2 float;
+        theta float;
+        rad_theta float;
+    BEGIN
+        IF lat1 = lat2 AND lon1 = lon2
+        THEN
+            RETURN dist;
+        ELSE
+            -- переводим градусы широты в радианы
+            rad_lat1 = pi() * lat1 / 180;
+            -- переводим градусы долготы в радианы
+            rad_lat2 = pi() * lat2 / 180;
+            -- находим разность долгот
+            theta = lon1 - lon2;
+            -- переводим градусы в радианы
+            rad_theta = pi() * theta / 180;
+            -- находим длину ортодромии
+            dist = sin(rad_lat1) * sin(rad_lat2) + cos(rad_lat1) * cos(rad_lat2) * cos(rad_theta);
+
+            IF dist > 1
+            THEN dist = 1;
+            END IF;
+
+            dist = acos(dist);
+            -- переводим радианы в градусы
+            dist = dist * 180 / pi();
+            -- переводим градусы в километры
+            dist = dist * 60 * 1.8524;
+
+            RETURN dist;
+        END IF;
+    END;
+'
+    LANGUAGE PLPGSQL;
